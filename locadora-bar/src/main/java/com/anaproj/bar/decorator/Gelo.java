4d12c1eb210396f9be76c854e92489a7cb7776a7
Lucas Medeiros <lmsantos@inf.ufsm.br>
2025-11-27 22:16:22 -0300

    exceção de instância de cigarro

--- locadora-bar/src/main/java/com/anaproj/bar/decorator/Gelo.java
+++ locadora-bar/src/main/java/com/anaproj/bar/decorator/Gelo.java
@@ -1,11 +1,15 @@
 package com.anaproj.bar.decorator;
 
+import com.anaproj.bar.factory.Cigarro;
 import com.anaproj.bar.factory.Produto;
 
 public class Gelo extends DecoradorProduto {
 
     public Gelo(Produto produto) {
         super(produto);
+        if (produto instanceof Cigarro) {
+            throw new IllegalArgumentException("Não é possível adicionar Gelo a um Cigarro!");
+        }
     }
 
     @Override
--- locadora-bar/src/main/java/com/anaproj/bar/decorator/Limao.java
+++ locadora-bar/src/main/java/com/anaproj/bar/decorator/Limao.java
@@ -1,11 +1,15 @@
 package com.anaproj.bar.decorator;
 
+import com.anaproj.bar.factory.Cigarro;
 import com.anaproj.bar.factory.Produto;
 
 public class Limao extends DecoradorProduto {
 
     public Limao(Produto produto) {
         super(produto);
+        if (produto instanceof Cigarro) {
+            throw new IllegalArgumentException("Não é possível adicionar Limão a um Cigarro!");
+        }
     }
 
     @Override
